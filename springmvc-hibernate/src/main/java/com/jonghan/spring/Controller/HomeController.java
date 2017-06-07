eaa79ab8356d92a57b600893e129b8fff737612b
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-06-07 14:51:44 +0900

    add logging module
    add logging module

--- springmvc-hibernate/src/main/java/com/jonghan/spring/Controller/HomeController.java
+++ springmvc-hibernate/src/main/java/com/jonghan/spring/Controller/HomeController.java
@@ -4,8 +4,8 @@ import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,7 +17,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+//	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+    private static final Logger logger = LogManager.getLogger(HomeController.class.getName());
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
