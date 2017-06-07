3005d2ba3d7fbdc99d32b2c553fe02fd6930a523
jonghan.kim <jonghan.kim@cdnetworks.co.kr>
2017-06-07 11:10:27 +0900

    configuration files
    configuration files

--- springmvc-hibernate/src/main/java/com/jonghan/spring/Config/MvcConfig.java
+++ springmvc-hibernate/src/main/java/com/jonghan/spring/Config/MvcConfig.java
@@ -1,10 +1,13 @@
 package com.jonghan.spring.Config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 /**
  * Created by jonghan on 17. 6. 7.
@@ -19,5 +22,17 @@ public class MvcConfig extends WebMvcConfigurerAdapter{
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
-    
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver bean = new InternalResourceViewResolver();
+        bean.setPrefix("/WEB-INF/views");
+        bean.setSuffix(".jsp");
+        return bean;
+    }
 }
